d798f59b244be101ad107270e1144614492791a5
leedaehee12 <sprinter239@gmail.com>
2022-09-27 21:04:07 +0900

    BaseTime Test

--- src/main/java/com/javateam/springboot/service/posts/PostsService.java
+++ src/main/java/com/javateam/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.javateam.springboot.service.posts;
 
 import com.javateam.springboot.web.domain.posts.PostRepository;
+import com.javateam.springboot.web.domain.posts.Posts;
+import com.javateam.springboot.web.dto.PostsResponseDto;
 import com.javateam.springboot.web.dto.PostsSaveRequestDto;
+import com.javateam.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +17,16 @@ public class PostsService {
     public long save(PostsSaveRequestDto requestDto){
         return postRepository.save(requestDto.toEntity()).getId();
     }
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당게시글이 없습니다. id=" + id));
 
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+    public PostsResponseDto findById(Long id){
+        Posts entity= postRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
+        return new PostsResponseDto(entity);
+    }
 }
