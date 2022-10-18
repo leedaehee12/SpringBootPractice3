15a36673bdaa8df5e2e993d49a62332843143d75
leedaehee12 <sprinter239@gmail.com>
2022-10-18 21:23:24 +0900

    oauth

--- src/main/java/com/javateam/springboot/service/posts/PostsService.java
+++ src/main/java/com/javateam/springboot/service/posts/PostsService.java
@@ -41,4 +41,11 @@ public class PostsService {
                 .map(PostsListResponseDto::new)
                 .collect(Collectors.toList());
     }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id=" +id));
+        postRepository.delete(posts);
+    }
 }
